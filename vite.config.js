7e705987cf59870bcd9a45f2fe737527cc3954bd
uku <hi@uku.moe>
2026-01-05 19:16:58 +0100

    chore: remove dist and some unneded stuff

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,8 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-const newLocal = '/'
 // https://vite.dev/config/
 export default defineConfig({
-  // publicPath: newLocal,
-  base: '/mcsr-widget/',
   transpileDependencies: true,
   plugins: [vue(), vueDevTools()],
   resolve: {
